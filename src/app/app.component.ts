da638f9d27593880974d0c5c8e84bd6e0b651f35
ANJD <ite.anjd@gmail.com>
2020-05-18 15:57:05 +0300

    some design improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+declare var VANTA;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'appointments-app';
+export class AppComponent implements OnInit {
+  title = 'Appointments App';
+
+  ngOnInit(): void {
+    // VANTA.HALO({
+    //   el: "body",
+    //   mouseControls: true,
+    //   touchControls: true,
+    //   minHeight: 200.00,
+    //   minWidth: 200.00
+    // });
+  }
 }
